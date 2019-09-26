bcd638258ffeb206e8c4ed250997a4f8013dfbf4
kurihara-nbo <kurihara.nbo@gmail.com>
2019-09-26 01:05:35 +0000

    update onelinebbs.php

--- onelinebbs/onelinebbs.php
+++ onelinebbs/onelinebbs.php
@@ -26,21 +26,17 @@ try{
 
 //データが入力されDBが接続されていた場合にデータを追加
 if ($db_connect && $data_exists) {
-    $sql = $pdo -> prepare("INSERT INTO post (name, comment) VALUES (:name, :comment)");
-    $sql->bindParam(':name', $name, PDO::PARAM_STR);
-    $stmt->bindValue(':comment', $comment, PDO::PARAM_STR);
-
-    $name = '';
-    if ($sql->execute();){
-        echo 'データを追加しました';
+    $sql = $pdo->prepare ( 'insert into post (name, comment) values (?, ?)' );
+    if ($sql->execute([$name, $comment])) {
+        echo 'データを追加しました。';
     }
-        else {
+     else {
          echo 'データを追加できませんでした';
-    }
+     }
 }
-    else {
-        echo 'POSTデータが存在しません';
-    }
+else {
+         echo 'POSTデータが存在しません';
+}
 
 ?>
 
@@ -54,11 +50,17 @@ if ($db_connect && $data_exists) {
     <body>
         <h1>ひとこと掲示板</h1>
         
-        <form action="" method="post">
-            名前：<input type="text" name="name"/><br />
-            ひと言:<input type="text" name="comment" size="60" /><br />
-            <input type="submit" name="submit" value="送信"/>
-        </form>
+        <form method="POST">
+    <label for="">
+        名前
+    </label>
+    <input type="text" name="name"/>
+    <label for="">
+        コメント
+    </label>
+    <input type="textarea" name="comment"/>
+    <input type="submit" value="投稿する"/>
+</form>
         
        <h2>投稿されたデータ一覧</h2>
         <table style="border: 1px solid #000;">
@@ -70,19 +72,12 @@ if ($db_connect && $data_exists) {
                 コメント
             </th>
         </tr>
-        <?php
-        $sql = $pdo->query("SELECT * FROM post ORDER BY no ASC");
-        while($row = $sql -> fetch(PDO::FETCH_ASSOC)) {
-        $name = $row["name"];
-        $comment = $row["comment"];
-        $create_at = $row["create_at"];
-echo<<<EOF
-
-ヒアドキュメント内の表示部分
-
-EOF;
-}
-?>
+         <?php foreach ( $pdo->query ( 'select * from post' ) as $row) :?>
+        <tr>
+            <td><?php echo $row['name'] ?></td>
+            <td><?php echo $row['comment'] ?></td>
+        </tr>
+    <?php endforeach; ?>
         </table>
     </body>
 </html>
